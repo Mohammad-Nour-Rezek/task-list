ad140932cd053d134c3d90c7d0fa19ddd2e24a14
Mohammad Nour Rezek <Eng.M_Nour@outlook.com>
2021-10-02 23:50:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = 'Nour';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Container">
+      <header>
+        <h1>Task List Component</h1>
+        <h2>Hello {name}</h2>
       </header>
     </div>
   );
