eb41a5b322bd6ce364e0d3c0d698ffd871cce735
Mohammad Nour Rezek <40129269+Mohammad-Nour-Rezek@users.noreply.github.com>
2021-10-03 22:37:21 +0300

    add tasks comp

---
+++ src/Components/Tasks.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const Task = () => {
+    const [tasks, setTasks] = useState([
+            {
+                id: 1,
+                text: 'Doctors Appointment',
+                day: 'Feb 5th at 2:30pm',
+                reminder: true
+            },    
+            {
+                id: 2,
+                text: 'Meeting at School',
+                day: 'Feb 6th at 1:30pm',
+                reminder: true
+            },
+            {
+                id: 3,
+                text: 'Food Shopping',
+                day: 'Feb 5th at 2:30pm',
+                reminder: false
+            }
+        ]);
+
+    return (        
+        <>
+         {tasks.map(task => (
+                <h3 key={tasks.id}>task.text</h3>
+            )
+         )}   
+        </>
+    )
+}
+
+export default Task
